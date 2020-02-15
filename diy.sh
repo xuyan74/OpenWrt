fe3e2f075755da1a4a179a3c7a7bc46ae5a0d964
xuyan74 <48081620+xuyan74@users.noreply.github.com>
2020-02-15 14:01:21 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,5 +1,5 @@
 echo '修改机器名称'
-sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
 
 #echo '修改网关地址'
 #sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
@@ -18,14 +18,14 @@ rm -rf ./package/lean/luci-theme-argon
 ln -s ../../../luci-theme-argon ./package/lean/
 
 echo '修改wifi名称'
-sed -i 's/OpenWrt/G-DOCK/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/OpenWrt/G-DOCK/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 echo '修改banner'
-rm -rf package/base-files/files/etc/banner
-cp -f ../banner package/base-files/files/etc/
+#rm -rf package/base-files/files/etc/banner
+#cp -f ../banner package/base-files/files/etc/
 
 echo '下载ServerChan'
-git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverchan
+#git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverchan
 
 echo '集成diy目录'
-ln -s ../../diy ./package/openwrt-packages
+#ln -s ../../diy ./package/openwrt-packages
